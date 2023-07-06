11b3ded7f2a370bc60ecf6f670a06c2c17dd1798
EugeBr <eugebordenave@gmail.com>
2023-07-06 14:14:36 +0200

    Add input elements

--- src/app/pages/demo/pages/shared/shared.component.ts
+++ src/app/pages/demo/pages/shared/shared.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-shared',
   templateUrl: './shared.component.html',
   styleUrls: ['./shared.component.css']
 })
-export class SharedComponent {
+export class SharedComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      input: [null]
+    })
+  }
+
+  onPatchValue() {
+    this.form.patchValue({input: 'some value'});
+  }
+
+  onSubmit(): void {
+    console.log('Presiono boton submit');
+    
+  }
 
 }
--- src/app/pages/demo/pages/shared/shared.module.ts
+++ src/app/pages/demo/pages/shared/shared.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './shared.component';
-import { ButtonsModule } from 'src/app/shared';
+import { ButtonsModule, ControlsModule } from 'src/app/shared';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { ButtonsModule } from 'src/app/shared';
   imports: [
     CommonModule,
     SharedRoutingModule,
-    ButtonsModule
+    ButtonsModule,
+    ControlsModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
