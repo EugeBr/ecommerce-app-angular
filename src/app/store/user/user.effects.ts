e3c605c4c02b28c5487bfaaaffdb17fffa170aca
EugeBr <eugebordenave@gmail.com>
2023-07-28 12:28:42 +0200

    Add Login effects

--- src/app/store/user/user.effects.ts
+++ src/app/store/user/user.effects.ts
@@ -1,15 +1,15 @@
 import { Injectable } from "@angular/core";
-import {createEffect, Actions, ofType} from '@ngrx/effects';
+import { createEffect, Actions, ofType } from '@ngrx/effects';
 import { Router } from "@angular/router";
 
-import {AngularFireAuth} from '@angular/fire/auth';
-import {AngularFirestore} from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import firebase from 'firebase/app';
 
 import { Observable, from, of } from "rxjs";
 import { map, switchMap, catchError, take, tap, withLatestFrom } from "rxjs/operators";
 
-import {environment} from '@src/environments/environments';
+import { environment } from '@src/environments/environments';
 
 import { User } from "./user.models";
 
@@ -26,13 +26,13 @@ export class UserEffects {
         private afAuth: AngularFireAuth,
         private afs: AngularFirestore,
         private router: NotificationService
-    ){}
+    ) { }
 
-    signUpEmail: Observable<Action> = createEffect(() => 
-        this.actions.pipe>(
+    signUpEmail: Observable<Action> = createEffect(() =>
+        this.actions.pipe > (
             ofType(fromActions.Types.SIGN_UP_EMAIL),
-            map((action: fromActions.SignInEmail) => action.credentials),
-            switchMap(credentials => 
+            map((action: fromActions.SignUpEmail) => action.credentials),
+            switchMap(credentials =>
                 from(this.afAuth.createUserWithEmailAndPassword(credentials.email, credentials.password)).pipe(
                     tap(() => {
                         firebase.auth().currentUser?.sendEmailVerification(
@@ -40,6 +40,25 @@ export class UserEffects {
                         );
                     }),
                     map((signUpState) => new fromActions.SignUpEmailSuccess(signUpState.user ? signUpState.user.uid : '')),
+                    catchError(err => {
+                        return of(new fromActions.SignUpEmailError(err.message))
+                    })
+                )
+            )
+        )
+    );
+
+    signInEmail: Observable<Action> = createEffect(() =>
+        this.actions.pipe > (
+            ofType(fromActions.Types.SIGN_IN_EMAIL),
+            map((action: fromActions.SignInEmail) => action.credentials),
+            switchMap(credentials =>
+                from(this.afAuth.signInWithEmailAndPassword(credentials.email, credentials.password)).pipe(
+                    switchMap(singInState =>
+                        this.afs.doc<User>(`users/${singInState.user ? singInState.user?.uid : ''}`).valueChanges().pipe(
+                            take(1),
+                            map(user => new fromActions.SignInEmailSuccess(singInState.user ? singInState.user?.uid : '', user || null))
+                        )),
                     catchError(err => {
                         return of(new fromActions.SignUpEmailError(err.message))
                     })
