01dcc961bb234cc67a151a4134096264325db6b0
EugeBr <eugebordenave@gmail.com>
2023-08-25 12:35:34 +0200

    Add user selectors

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -2,12 +2,16 @@ import { ActionReducerMap } from "@ngrx/store";
 
 import * as fromDictionaries from './dictionaries';
 
+import * fromUser from './user';
+
 export interface State {
     dictionaries: fromDictionaries.DictionariesState;
+    user: fromUser.UserState;
 }
 
 export const reducers: ActionReducerMap<State> = {
-    dictionaries: fromDictionaries.reducer
+    dictionaries: fromDictionaries.reducer,
+    user: fromUser.reducer
 };
 
 export const effects = [
