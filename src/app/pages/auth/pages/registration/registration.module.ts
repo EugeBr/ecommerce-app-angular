ab987bfb4f8679583ceaba5cff5055f535436099
EugeBr <eugebordenave@gmail.com>
2023-08-28 12:10:15 +0200

    Create validations in register form

--- src/app/pages/auth/pages/registration/registration.module.ts
+++ src/app/pages/auth/pages/registration/registration.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { RegistrationRoutingModule } from './registration-routing.module';
 import { RegistrationComponent } from './registration.component';
 
+import { FormFieldModule, InputModule, PasswordModule } from 'src/app/shared/controls';
+import { ButtonModule } from 'src/app/shared/buttons';
+import { SpinnerModule } from 'src/app/shared/indicators';
 
 @NgModule({
   declarations: [
@@ -11,7 +17,14 @@ import { RegistrationComponent } from './registration.component';
   ],
   imports: [
     CommonModule,
-    RegistrationRoutingModule
+    RegistrationRoutingModule,
+    RouterModule,
+    ReactiveFormsModule,
+    FormFieldModule,
+    InputModule,
+    PasswordModule,
+    ButtonModule,
+    SpinnerModule
   ]
 })
 export class RegistrationModule { }
