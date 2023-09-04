bcd78f15b9aca5a826c537fb035f38d7f2984592
EugeBr <eugebordenave@gmail.com>
2023-09-04 14:15:46 +0200

    Add file selection and storage

--- src/app/shared/popups/files-upload/files-upload.component.ts
+++ src/app/shared/popups/files-upload/files-upload.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  multiple: boolean;
+  crop: boolean;
+}
 
 @Component({
   selector: 'app-files-upload',
@@ -8,9 +14,42 @@ import { Component } from '@angular/core';
 export class FilesUploadComponent {
   isHovering !: boolean;
 
-  constructor() {}
+  files : File[] = [];
+  imageFile!: File;
+  isError!: boolean;
 
-  toggleHover(event: boolean) {
+  filesURLs : string[] = [];
+
+  constructor(
+    private dialogRef: MatDialogRef<FilesUploadComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+
+  toggleHover(event: boolean | any) {
     this.isHovering = event;
+  }
+
+  onDrop(files: FileList | any): void {
+    this.dropGeneral(files);
+  }
+
+  onDropFile(files: FileList | any): void {
+    this.dropGeneral(event.target.files);
+  }
+
+  dropGeneral(files: FileList): void {
+    this.isError = false;
+
+    if(this.data.crop && files.length>1) {
+      this.isError = true;
+      return;
+    }
+
+    for(let i=0; i < files.length; i++) {
+      this.files.push(files.item(i) as File);
+    }
+
+    console.log(files);
+    
   }
 }
