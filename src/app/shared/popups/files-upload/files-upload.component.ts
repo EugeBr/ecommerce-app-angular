b857bb6044da92275c8bdf8b75c12c57cd78f201
EugeBr <eugebordenave@gmail.com>
2023-09-04 09:07:45 +0200

    Add control upload structure

---
+++ src/app/shared/popups/files-upload/files-upload.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-files-upload',
+  templateUrl: './files-upload.component.html',
+  styleUrls: ['./files-upload.component.css']
+})
+export class FilesUploadComponent {
+
+}
