aac71d5a5ac4e87f10cfb565390d388cb3325f47
EugeBr <eugebordenave@gmail.com>
2023-09-11 14:28:58 +0200

    Read data from json file

--- src/app/store/dictionaries/dictionaries.models.ts
+++ src/app/store/dictionaries/dictionaries.models.ts
@@ -2,10 +2,11 @@ import { Item, ControlItem, Icon } from "@app/models/frontend";
 export { Item, ControlItem } from "@app/models/frontend";
 
 export interface Dictionaries {
-    roles: Dictionary,
-    specializations: Dictionary,
-    qualifications: Dictionary,
-    skills: Dictionary
+    roles: Dictionary;
+    specializations: Dictionary;
+    qualifications: Dictionary;
+    skills: Dictionary;
+    countries: Dictionary;
 }
 
 export interface Dictionary {
