2bcceb5e7697c65d4f1e77004ad96aa95371a774
EugeBr <eugebordenave@gmail.com>
2023-09-11 14:38:22 +0200

    Read data from store

--- src/app/pages/profile/pages/form/form.component.ts
+++ src/app/pages/profile/pages/form/form.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { StepperService } from './components/stepper/services';
-import { Subject, takeUntil } from 'rxjs';
+import { Observable, Subject, takeUntil } from 'rxjs';
+
+import { Store, select } from '@ngrx/store';
+import * as fromRoot from 'src/app/store';
+import * as fromDictionaries from 'src/app/store/dictionaries';
 
 @Component({
   selector: 'app-form',
@@ -9,13 +13,19 @@ import { Subject, takeUntil } from 'rxjs';
 })
 export class FormComponent implements OnInit, OnDestroy{
 
+  dictionaries$!: Observable<fromDictionaries.Dictionaries>;
+  dictionariesIsReady$!: Observable<boolean>;
+
   private destroy = new Subject<any>();
 
   constructor(
-    public stepper: StepperService
+    public stepper: StepperService,
+    private store: Store<fromRoot.State>
   ) {}
 
   ngOnInit(): void {
+    this.dictionaries$ = this.store.pipe(select(fromDictionaries.getDictionaries)) as Observable<any>;
+    this.dictionariesIsReady$ = this.store.pipe(select(fromDictionaries.getIsReady)) as Observable<boolean>;
     this.stepper.init([
       {key: 'personal', label: 'Personal'},
       {key: 'professional', label: 'Profesional'}
