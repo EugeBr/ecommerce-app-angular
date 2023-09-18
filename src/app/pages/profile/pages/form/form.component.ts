30bdbe803ebeb9cafca1904befbdcbe8c5448efc
EugeBr <eugebordenave@gmail.com>
2023-09-18 12:25:40 +0200

    Resolve user

--- src/app/pages/profile/pages/form/form.component.ts
+++ src/app/pages/profile/pages/form/form.component.ts
@@ -5,8 +5,10 @@ import { Observable, Subject, takeUntil } from 'rxjs';
 import { Store, select } from '@ngrx/store';
 import * as fromRoot from 'src/app/store';
 import * as fromDictionaries from 'src/app/store/dictionaries';
+import * as fromUser from 'src/app/store/user';
 import { PersonalForm } from './components/personal/personal.component';
 import { ProfesionalForm } from './components/professional/professional.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 export interface ProfileForm {
   personal: PersonalForm | null;
@@ -18,19 +20,26 @@ export interface ProfileForm {
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit, OnDestroy{
+export class FormComponent implements OnInit, OnDestroy {
 
   dictionaries$!: Observable<fromDictionaries.Dictionaries>;
   dictionariesIsReady$!: Observable<boolean>;
 
   private destroy = new Subject<any>();
 
+  private user!: fromUser.User;
+
   constructor(
+    private router: Router,
+    private route: ActivatedRoute,
     public stepper: StepperService,
     private store: Store<fromRoot.State>
   ) {}
 
   ngOnInit(): void {
+
+    this.user = this.route.snapshot.data.user;
+
     this.dictionaries$ = this.store.pipe(select(fromDictionaries.getDictionaries)) as Observable<any>;
     this.dictionariesIsReady$ = this.store.pipe(select(fromDictionaries.getIsReady)) as Observable<boolean>;
     this.stepper.init([
